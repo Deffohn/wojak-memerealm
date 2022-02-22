061d555527961021aae023f4cd42d0eb6086d20a
Hugo Cascio <cascio.hugo@gmail.com>
2022-02-22 19:04:41 +0100

    Account Settings UI

--- app/src/main/java/fr/stks/wojakmemesrealm/fragments/ProfileFragment.kt
+++ app/src/main/java/fr/stks/wojakmemesrealm/fragments/ProfileFragment.kt
@@ -1,11 +1,16 @@
 package fr.stks.wojakmemesrealm.fragments
 
+
+
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import fr.stks.wojakmemesrealm.AccountSettingsActivity
 import fr.stks.wojakmemesrealm.R
+import fr.stks.wojakmemesrealm.databinding.FragmentProfileBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,11 +21,18 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  */
 class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentProfileBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        binding.editAccountSettingsBtn.setOnClickListener {
+            startActivity(Intent(context, AccountSettingsActivity::class.java))
+        }
+
+        return binding.root
     }
 }
