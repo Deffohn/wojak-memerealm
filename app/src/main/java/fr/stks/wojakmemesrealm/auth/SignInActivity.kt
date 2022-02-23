c2a806c4aaa013b2e9339bee47f773bf6bae0b30
vecil <69813615+vecil@users.noreply.github.com>
2022-02-23 16:24:27 +0100

    User registration with Firebase

--- app/src/main/java/fr/stks/wojakmemesrealm/auth/SignInActivity.kt
+++ app/src/main/java/fr/stks/wojakmemesrealm/auth/SignInActivity.kt
@@ -3,6 +3,8 @@ package fr.stks.wojakmemesrealm.auth
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import fr.stks.wojakmemesrealm.MainActivity
 import fr.stks.wojakmemesrealm.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
@@ -14,6 +16,22 @@ class SignInActivity : AppCompatActivity() {
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupListeners()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        FirebaseAuth.getInstance().currentUser?.let {
+            Intent(this@SignInActivity, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(this)
+            }
+            finish()
+        }
+    }
+
+    private fun setupListeners() {
         binding.signupLinkBtn.setOnClickListener {
             Intent(this, SignUpActivity::class.java).run {
                 startActivity(this)
--- app/src/main/java/fr/stks/wojakmemesrealm/auth/SignUpActivity.kt
+++ app/src/main/java/fr/stks/wojakmemesrealm/auth/SignUpActivity.kt
@@ -1,8 +1,14 @@
 package fr.stks.wojakmemesrealm.auth
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import fr.stks.wojakmemesrealm.MainActivity
 import fr.stks.wojakmemesrealm.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
@@ -14,9 +20,84 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupListeners()
+    }
+
+    private fun setupListeners() {
         binding.signinLinkBtn.setOnClickListener {
             Intent(this, SignInActivity::class.java).run {
                 startActivity(this)
+            }
+        }
+
+        binding.signupBtn.setOnClickListener {
+            createAccount()
+        }
+    }
+
+    private fun createAccount() {
+        val fullName = binding.fullnameSignup.text.toString()
+        val username = binding.usernameSignup.text.toString()
+        val mail = binding.mailSignup.text.toString()
+        val password = binding.passwordSignup.text.toString()
+
+        when {
+            TextUtils.isEmpty(fullName) -> Toast.makeText(this, "Full name is required!", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(username) -> Toast.makeText(this, "Username is required!", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(mail) -> Toast.makeText(this, "Mail is required!", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(password) -> Toast.makeText(this, "Password is required!", Toast.LENGTH_LONG).show()
+
+            else -> {
+                val progressDialog = ProgressDialog(this@SignUpActivity)
+                progressDialog.apply {
+                    setTitle("SignUp")
+                    setMessage("Welcome among us! Please wait a bit...")
+                    setCanceledOnTouchOutside(false)
+                    show()
+                }
+
+                val fAuth = FirebaseAuth.getInstance()
+                fAuth.createUserWithEmailAndPassword(mail, password).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        saveUserInfo(fullName, username, mail, progressDialog)
+                    } else {
+                        val error = task.exception.toString()
+                        Toast.makeText(this, "Error: $error.", Toast.LENGTH_LONG).show()
+                        progressDialog.dismiss()
+                        fAuth.signOut()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun saveUserInfo(fullName: String, username: String, mail: String, progressDialog: ProgressDialog) {
+        val fAuth = FirebaseAuth.getInstance()
+        val dbUsers = FirebaseDatabase.getInstance().reference.child("Users")
+
+        val uid = fAuth.currentUser!!.uid
+        dbUsers.child(uid).setValue(HashMap<String, Any>().apply {
+            put("uid", uid)
+            put("fullname", fullName)
+            put("username", username)
+            put("mail", mail)
+            put("image", "https://firebasestorage.googleapis.com/v0/b/wojak-memes-realm.appspot.com/o/Default%20Images%2Fprofile.png?alt=media&token=e8d07ce3-99a4-4b3c-bd63-1037511ee086")
+            put("bio", "")
+        }).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                progressDialog.dismiss()
+                Toast.makeText(this, "Your account has been created sucessfully.", Toast.LENGTH_SHORT).show()
+
+                Intent(this@SignUpActivity, MainActivity::class.java).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(this)
+                }
+                finish()
+            } else {
+                val error = task.exception.toString()
+                Toast.makeText(this, "Error: $error.", Toast.LENGTH_LONG).show()
+                progressDialog.dismiss()
+                fAuth.signOut()
             }
         }
     }
