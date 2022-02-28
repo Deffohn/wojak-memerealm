5c02bac43e34d9136b71f4165b2c9607d34cee59
deffohn <deffohn@pop-os.localdomain>
2022-02-28 16:59:15 +0100

    postAdapter config for Posts and post layout

--- app/src/main/java/fr/stks/wojakmemesrealm/fragments/HomeFragment.kt
+++ app/src/main/java/fr/stks/wojakmemesrealm/fragments/HomeFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import fr.stks.wojakmemesrealm.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -16,11 +18,23 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        var recyclerView: RecyclerView? = null
+        recyclerView = view.findViewById(R.id.recycler_view_home)
+        val linearLayoutManager = LinearLayoutManager(context)
+        linearLayoutManager.reverseLayout = true
+        linearLayoutManager.stackFromEnd = true
+        recyclerView.layoutManager = linearLayoutManager
+
+
+
+        return view
     }
 }
